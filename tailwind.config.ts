05c99bd86d0ad3ddd6b95898b47b222da0a9f28d
Md Zishan Alam <zishanalam752@gmail.com>
2024-04-17 17:03:52 +0530

    tailwind_config file bugs removed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,9 +16,6 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'hero': "url('../src/assets/contactusbg.jpg')",
-      },
       boxShadow: {
         input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
       },
@@ -60,6 +57,7 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          'hero': "url('../src/assets/contactusbg.jpg')",
       },
     },
   },
